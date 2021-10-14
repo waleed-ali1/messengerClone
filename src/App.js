17b113db34af9881d2e48e1e640d626c602a595d
walid <walidkenaan10@gmail.com>
2021-10-14 22:40:50 +0300

    adding the input and the button and display the message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import useEffect from 'react';
 import './App.css';
+import Button /*, FormControl, InputLabel, Input }*/ from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import  InputLabel from '@material-ui/core/InputLabel';
+import  Input from '@material-ui/core/Input';
+import Message from './Components/Message';
+
+
+
  
 class App extends Component {
+
  
     state= {
       input :"",
-      messages : []
+      messages : [],
+      username:""
     }
 
+
+
+     
     inputHandler = (event ) =>{
 
       this.setState({
@@ -17,50 +31,67 @@ class App extends Component {
     }
 
      
-    sendMessage = () => {
+    sendMessage = (event) => {
 
+      event.preventDefault();
       const newMessagesArray = [...this.state.messages];
       newMessagesArray.push(this.state.input);
       this.setState({
-        messages : newMessagesArray
+        messages : newMessagesArray,
+        input:""
       });
       // return newMessagesArray;
       console.log(newMessagesArray);
 
     }
 
-   
+    // ComponentDidMount = ()=>{
+
+    //   const user = prompt('Enter your username');
+    //   this.setState({
+    //     username : user 
+    //   })
+    //   console.log('yes');
+    // }
+
+    
+  //   useEffect = (()=>{
+
+  //   const user = prompt('Enter your username');
+  //   this.setState({
+  //     username : user 
+  //   })
+
+  // },[])
+
+    // choosingUser = () =>{
+    //   const user = prompt('enter you username ');
+    //   this.setState({
+    //     username:user
+    //   })
+    // }
+      
   render() {
 
-    // const messagesList = () =>{
-
-    //   this.state.messages.map(msg =>{
-    //     return(
-    //       <p>{msg}</p>
-    //     )
-    //   })
-
-    // }
 
     return (
       <div className="App">
       
-      <form> 
-      <input type="text" value={this.state.input} onChange={this.inputHandler}/>
-
-      <button onClick={this.sendMessage}> Send Message</button>
-
+      <form>
+          <FormControl>
+            <InputLabel > Enter a message ... </InputLabel>
+            <Input value={this.state.input} onChange={this.inputHandler} />
+            <Button disabled={!this.state.input} variant="contained" onClick={this.sendMessage} color="primary" type="submit">Outlined</Button>
+          </FormControl>
       </form>
-
-        {this.state.messages.map( msg => {
-          return(
-          <p>{msg}</p>
-          )
-        }) }
-      
   
-
-
+      {
+        this.state.messages.map(msg => {
+          return(
+            <Message text={msg}/>
+          )
+        })
+      }
 
       </div>
     );
---
+++ src/Components/Message.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Message = (props) => {
+    return (
+        <div>
+          <h3>{props.text}</h3>  
+        </div>
+    )
+}
+
+export default Message
