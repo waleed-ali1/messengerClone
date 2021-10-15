cb3576907309f97836bb1941931f89e6f1df7899
walid <walidkenaan10@gmail.com>
2021-10-15 21:03:39 +0300

    finishing up the msgs styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import useEffect from 'react';
+import React,  { Component } from 'react';
 import './App.css';
 import Button /*, FormControl, InputLabel, Input }*/ from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
@@ -14,12 +13,27 @@ class App extends Component {
 
  
     state= {
-      input :"",
-      messages : [],
-      username:""
+       input :"",
+       username:"",
+      messages : [
+        {
+          input:"heyy first object",
+          username:"walid"
+
+        },
+
+        {
+          input:"heyyy",
+          username:"ziad"
+
+        },
+        {
+          input:"heyyy",
+          username:"mhamad"
+
+        }
+      ]
     }
-
-
 
      
     inputHandler = (event ) =>{
@@ -34,63 +48,60 @@ class App extends Component {
     sendMessage = (event) => {
 
       event.preventDefault();
-      const newMessagesArray = [...this.state.messages];
-      newMessagesArray.push(this.state.input);
+     // const newArrObject = [...this.state.messages];  // new array of objects
+      //console.log(newObject);
+      // const newMessagesArray = [...this.state.messages];
+      // newMessagesArray.push(this.state.messages);
+      let newUsername = this.state.username;
+      let inputUser = this.state.input;
+      const newObj = [...this.state.messages];
+      newObj.push({input:inputUser,
+                    username:newUsername
+                    });
+      //newArrObject.push(this.state.messages);
       this.setState({
-        messages : newMessagesArray,
+        messages : newObj,
         input:""
       });
+
       // return newMessagesArray;
-      console.log(newMessagesArray);
+      console.log(newObj);
 
     }
 
-    // ComponentDidMount = ()=>{
 
-    //   const user = prompt('Enter your username');
-    //   this.setState({
-    //     username : user 
-    //   })
-    //   console.log('yes');
-    // }
+    componentWillMount = () => {
 
-    
-  //   useEffect = (()=>{
+      const user = prompt('Enter your username');
+      this.setState({
+        username : user 
+      })
+    }
 
-  //   const user = prompt('Enter your username');
-  //   this.setState({
-  //     username : user 
-  //   })
-
-  // },[])
-
-    // choosingUser = () =>{
-    //   const user = prompt('enter you username ');
-    //   this.setState({
-    //     username:user
-    //   })
-    // }
+     
       
   render() {
 
 
     return (
       <div className="App">
+        
+        <h2 className="greeting"> Welcome {this.state.username}</h2>
       
       <form>
           <FormControl>
             <InputLabel > Enter a message ... </InputLabel>
             <Input value={this.state.input} onChange={this.inputHandler} />
-            <Button disabled={!this.state.input} variant="contained" onClick={this.sendMessage} color="primary" type="submit">Outlined</Button>
+            <Button disabled={!this.state.input} variant="contained" onClick={this.sendMessage} color="primary" type="submit">Send</Button>
           </FormControl>
       </form>
   
       {
         this.state.messages.map(msg => {
           return(
-            <Message text={msg}/>
+            <Message username={this.state.username} msg={msg}/>
           )
-        })
+        })  
       }
 
       </div>
--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import './Message.css';
 
 const Message = (props) => {
+
+
+  const isUser = props.username === props.msg.username;
     return (
-        <div>
-          <h3>{props.text}</h3>  
+        <div className={`message_card ${isUser && 'message_user'}`}>
+          <Card className={isUser ? "message__userCard" : "message__guestCard"} >
+            <CardContent>
+
+              <Typography color ="white"
+              variant="h5"
+              component="h2">
+                {props.msg.username} : {props.msg.input}
+                </Typography>
+
+            </CardContent>
+            </Card>
+         
         </div>
     )
 }
